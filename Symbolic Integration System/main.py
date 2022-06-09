e7f284aa13d8ba56737266f2a7f24e40d9bfc1ec
S-OS <33400784+sosudo@users.noreply.github.com>
2022-06-09 10:05:03 -0400

    Update main.py

--- Symbolic Integration System/main.py
+++ Symbolic Integration System/main.py
@@ -1,11 +1,19 @@
 import sympy
 sympy.init_printing()
+t, s = sympy.symbols('t, s')
 def derivative(f):
-  t, s = sympy.symbols('t, s')
   a = sympy.symbols('a', real=True, positive=True)
   F = sympy.laplace_transform(f, t, s, noconds=True)
   sFf = s*F-f.subs(t, 0)
   simplified = sympy.simplify(sFf)
   fprime = sympy.inverse_laplace_transform(simplified, s, t, noconds=True)
   return fprime
+def integral(f):
+  a = sympy.symbols('a', real=True, positive=True)
+  F = sympy.laplace_transform(f, t, s, noconds=True)
+  sFf = F/s-f.subs(t, 0)
+  simplified = sympy.simplify(sFf)
+  bigF = sympy.inverse_laplace_transform(simplified, s, t, noconds=True)
+  return bigF
 print(derivative(3*t**2))
+print(integral(3*t**2))
