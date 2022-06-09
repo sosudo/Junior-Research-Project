cd87f68b7f61838b3c30f7521db776a6bac5982f
S-OS <33400784+sosudo@users.noreply.github.com>
2022-06-09 10:01:18 -0400

    Update main.py

--- Symbolic Integration System/main.py
+++ Symbolic Integration System/main.py
@@ -1,9 +1,11 @@
 import sympy
 sympy.init_printing()
-t, s = sympy.symbols('t, s')
-a = sympy.symbols('a', real=True, positive=True)
-#f = sympy.exp(2*t)*sympy.sin(5*t)
-f = 4*t+1
-F = sympy.laplace_transform(f, t, s)
-print(F)
-print(s*F-1)
+def derivative(f):
+  t, s = sympy.symbols('t, s')
+  a = sympy.symbols('a', real=True, positive=True)
+  F = sympy.laplace_transform(f, t, s, noconds=True)
+  sFf = s*F-f.subs(t, 0)
+  simplified = sympy.simplify(sFf)
+  fprime = sympy.inverse_laplace_transform(simplified, s, t, noconds=True)
+  return fprime
+print(derivative(3*t**2))
