7e5772f4c96a5e5d9e9d93eb2c9780d3090f570f
S-OS <33400784+sosudo@users.noreply.github.com>
2022-07-06 14:32:57 -0400

    Update stft_modules.py

--- Algorithms Using the Fourier Transform/Short Time Fourier Transform/stft_modules.py
+++ Algorithms Using the Fourier Transform/Short Time Fourier Transform/stft_modules.py
@@ -4,7 +4,6 @@
 
 # Import the signal module, which includes the STFT function we need to make the comparison
 from scipy import signal
-import numpy as np
 # Calculates the mean squared error for a dataset with complex numbers, finding the magnitude instead of the absolute value
 def complex_MSE(data):
   sum = 0
